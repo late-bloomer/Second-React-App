9bf0c3e6d1ad828c12292e510bea3dd681bf0c0f
Mohit Sharma <mohit.sharma@target.com>
2019-02-03 13:04:55 +0530

    Second App Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import Table from './Table';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Table />
+      </div>
+    );
+  }
+}
+
+  
+
+
+
+export default App;
